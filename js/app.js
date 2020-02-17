d1872a0943b1e2194456f1762010126cecb0c5c0
ragadnajjar <ragadnajjar9@gmail.com>
2020-02-17 17:13:45 +0200

    ubdate

---
+++ js/app.js
@@ -0,0 +1,14 @@
+`use strict`;
+var name= prompt("what's your name");
+alert (" welcome " + name + " I'm happy that you're on my website ");
+console.log(name);
+var friend= prompt("do you wanna be my friend?");
+if (friend=="yes"){
+    alert("thank you :)");
+}
+console.log(friend);
+var age= prompt(" how old are you "+ name );
+console.log(age);
+var read= prompt(" do you like reading ? ");
+alert (" reading a very useful hobby,I like it too! ");
+console.log(read);
