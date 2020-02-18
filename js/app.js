4b81c43286fcbda0b256e556f2bb4e24e769d5d8
ragadnajjar <ragadnajjar9@gmail.com>
2020-02-18 14:06:42 +0200

    llll

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
 `use strict`;
 var name= prompt("what's your name");
 alert (" welcome " + name + " I'm happy that you're on my website ");
-console.log(name);
-var friend= prompt("do you wanna be my friend?");
+// console.log(name);
+var friend= prompt("do you wanna be my friend?").toUpperCase();
 if (friend=="yes"){
     alert("thank you :)");
 }
-console.log(friend);
-var age= prompt(" how old are you "+ name );
-console.log(age);
-var read= prompt(" do you like reading ? ");
-alert (" reading a very useful hobby,I like it too! ");
-console.log(read);
+// console.log(friend);
+var age= prompt(" how old are you "+ name ).toLowerCase();
+// console.log(age);
+var read= prompt(" do you like reading ? ").toUpperCase();
+if (read=="yes"){
+    alert (" reading a very useful hobby,I like it too! ");
+}
+// console.log(read);
+var fav= prompt('do you like Listening to music?').toLowerCase();
+if (fav=="yes"){
+    alert ("nice!")
+}
+if (fav=="no"){
+    alert ("you should try it sometimes");
+}
+// console.log(fav);
+
+
+
